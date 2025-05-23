84b3b20ae90f35fac43fcbfb88d5814935e0cd5f
mzulkifalzk25 <mzulkifalzk25@gmail.com>
2025-05-24 00:42:13 +0500

    Add environment configuration file and update example.env with new settings
    - Create a new .env file for sensitive configurations.
    - Update example.env with example values for database, API keys, email settings, and Celery configuration.
    - Modify .gitignore to ensure .env is not tracked.
    - Update core/settings.py to utilize environment variables for database settings.
    - Add mysql-connector-python to requirements.txt for MySQL support.

--- core/settings.py
+++ core/settings.py
@@ -118,23 +118,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME', 'bird_detection'),
-#         'USER': os.getenv('DB_USER', 'postgres'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),
-#         'PORT': os.getenv('DB_PORT', '5432'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
